4a09ff93c989d6231890a3d61c8af98f376e2ab3
Lenah Karimi <lenahkarimi18@gmail.com>
2025-10-15 13:02:55 +0300

    Prepare for deployment

--- micro_challenge_api/settings.py
+++ micro_challenge_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ht)%2c87jj2lr523b9u^zcy)^7xa&hk=!43*80z#(_mdv=g_ar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
